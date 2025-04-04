78e4dfc9e190333a2e1a2dfcc4ab7a5f9d28a6b8
Gautam-Lathiya <gautamlathiya18@gmail.com>
2025-04-05 01:11:50 +0530

    Implement functionalities for Task3 along with Unit Testing

---
+++ Test3.Tests/Controllers/HelloWorldControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Web.Mvc;
+using Test3.Controllers;
+
+namespace Test3.Tests.Controllers
+{
+    [TestClass]
+    public class HelloWorldControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            var controller = new HelloWorldController();
+
+            // Act
+            var result = controller.Index() as ContentResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Hello world", result.Content);
+        }
+    }
+}
